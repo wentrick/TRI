27ba3fc9eddc7dd3bc29e6cc50e26c194db988ce
Davi Wentrick Feijó <davi.w.feijo@gmail.com>
2023-09-11 00:00:28 -0300

    lista 1 tri

--- listas/lista1/lista1_TRI.R
+++ listas/lista1/lista1_TRI.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse,kable,irtoys,ltm,mirt)
+pacman::p_load(tidyverse,kableExtra,irtoys,ltm,mirt)
 
 altura <- read.fwf(file="listas/lista1/altura211.txt", widths=c(3,4,rep(1,14)),dec=',')
 
@@ -25,3 +25,53 @@ for (i in 1:14) {
 kable(data.frame(rho.PB, rho.B.vec), 
       col.names = c("Ponto-bisserial", "Bisserial"), align = "c")
 
+# Questao 2 
+
+plot(altura.desc$bisCorr, rho.B.vec, xlab=c("correlação ponto bisserial"),
+     ylab=c("correlação bisserial"),xlim=c(0,1),ylim=c(0,1))
+abline(0,1)
+
+
+# Questao 3 
+
+mu <- mean(altura$altura)
+desvio <- sd(altura$altura)
+
+t <- apply(altura.itens,1,sum)
+t_media <- mean(t)
+s <- sd(t)
+
+## escore padronizado 
+zj <- (t-t_media)/s
+
+## altura estimada
+hj <- mu + desvio*zj
+
+## correlação entre alturas reais e estimadas
+
+(cor(altura$altura, hj))
+
+plot(altura$altura, hj, xlab=c("altura real"),
+     ylab=c("estimada"))
+
+
+# Questao 4
+
+cronbach.alpha(altura.itens)
+
+
+# Questao 5
+
+(1-apply(altura.itens,2,sum)/nrow(altura.itens))
+
+# Questao 6
+
+escore <- apply(altura.itens,1,sum)
+aux <- ceiling(0.27*nrow(altura.itens))
+escore.inf <- sort(escore)[aux]
+escore.sup <- sort(escore)[nrow(altura.itens)-aux]
+
+altura.inf <- altura.itens[escore<=escore.inf,]
+altura.sup <- altura.itens[escore>=escore.sup,]
+
+apply(altura.sup,2,sum)/nrow(altura.sup)-apply(altura.inf,2,sum)/nrow(altura.inf)
